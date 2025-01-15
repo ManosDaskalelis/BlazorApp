89c1160fcd1dc23d88f30ae01b359f5736cf011e
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2025-01-15 22:53:31 +0200

    Complete cart functionality and order status added

---
+++ Utility/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Utility
+{
+    public static class OrderStatus
+    {
+        public static string StatusPending = "Pending";
+        public static string StatusDelivering = "Delivering";
+        public static string StatusDeliverd = "Delivered";
+        public static string StatusCancelled = "Cancelled";
+    }
+}
